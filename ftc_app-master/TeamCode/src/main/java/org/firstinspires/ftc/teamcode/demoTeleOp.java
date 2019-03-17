84afd877a8ca7c687c76332298c1edfd4cbe8a76
MatthewN <kiddiemjn@verizon.net>
2019-03-17 15:18:39 -0400

    Create demoTeleOp.java
    Created program for demo bot

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/demoTeleOp.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Demo Bot")
+public class demoTeleOp extends OpMode {
+    public DcMotor FLD                  = null; //Front Left Drive Motor, "FLD"
+    public DcMotor FRD                  = null; //Front Right Drive Motor, "FRD"
+    public void init() {
+        FLD = hardwareMap.get(DcMotor.class, "FLD");
+        FRD = hardwareMap.get(DcMotor.class, "FRD");
+
+        FLD.setDirection(DcMotor.Direction.REVERSE);
+        FRD.setDirection(DcMotor.Direction.FORWARD);
+
+        FLD.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FRD.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        FLD.setPower(0);
+        FRD.setPower(0);
+    }
+    @Override
+    public void loop(){
+        FLD.setPower(-gamepad1.left_stick_y);
+        FRD.setPower(-gamepad1.right_stick_y);
+    }
+}
