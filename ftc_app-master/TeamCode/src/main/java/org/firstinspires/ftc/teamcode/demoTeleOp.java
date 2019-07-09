703a8650285fb2608338773079a9792e9e628487
MatthewN <kiddiemjn@verizon.net>
2019-07-09 18:35:26 -0400

    Update demoTeleOp.java

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/demoTeleOp.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/demoTeleOp.java
@@ -6,24 +6,24 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 //geet test
 @TeleOp(name="Demo Bot") //tam
 public class demoTeleOp extends OpMode {
-    public DcMotor FLD                  = null; //Front Left Drive Motor, "FLD"
-    public DcMotor FRD                  = null; //Front Right Drive Motor, "FRD"
+    public DcMotor LD                  = null; //Front Left Drive Motor, "FLD"
+    public DcMotor RD                  = null; //Front Right Drive Motor, "FRD"
     public void init() {
-        FLD = hardwareMap.get(DcMotor.class, "FLD");
-        FRD = hardwareMap.get(DcMotor.class, "FRD");
+        LD = hardwareMap.get(DcMotor.class, "FLD");
+        RD = hardwareMap.get(DcMotor.class, "FRD");
 
-        FLD.setDirection(DcMotor.Direction.REVERSE);
-        FRD.setDirection(DcMotor.Direction.FORWARD);
+        LD.setDirection(DcMotor.Direction.REVERSE);
+        RD.setDirection(DcMotor.Direction.FORWARD);
 
-        FLD.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        FRD.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LD.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RD.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        FLD.setPower(0);
-        FRD.setPower(0);
+        LD.setPower(0);
+        RD.setPower(0);
     }
     @Override
     public void loop(){
-        FLD.setPower(-gamepad1.left_stick_y);
-        FRD.setPower(-gamepad1.right_stick_y);
+        LD.setPower(-gamepad1.left_stick_y);
+        RD.setPower(-gamepad1.right_stick_y);
     }
 }
